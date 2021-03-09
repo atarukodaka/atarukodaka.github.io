ddea1c1d50a2521ff084f7093f680a496d5d3f1a
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-03-09 23:30:53 +0900

    add-content

---
+++ content/posts/game/umamusume/umamusume.md
@@ -0,0 +1,48 @@
+---
+title: ウマ娘はじめました
+date: 2021-03-02
+cover: gorushi.png
+description: ウマ娘育成ゲーム始めてみた。
+---
+
+## 育成
+スピード重視短距離が育てやすいらしい。
+
+<Alert severity="info">スピード命！ハルウララ</Alert>
+
+## 殿堂入り
+とりあえずこんな感じ：
+
+![umamusume](./umamusume.png)
+
+## 娘たち
+
+### サクラバクシンオー☆１
+URAファイナル制覇。
+とにかくスピード命。休みの代わりに勉強かお出かけ。
+
+![sakurabakushinou](./sakurabakushinou.png)
+
+### スペシャルウィーク☆３
+スタミナ・パワー中心に育てて有馬記念優勝。URAファイナルは予選にて敗退。
+![specialweek](./specialweek.png)
+
+
+### ハルウララ☆１
+唯一のダート得意。有馬敗着。。。ごめんよ。。。
+URAファイナル進出も予選で敗退。
+
+![haruurara](./haruurara.jpg)
+
+### タイキシャトル☆３
+
+![taikishatoru](./taikishatoru.png)
+
+
+## 参考サイト
+
+- [ウマ娘 プリティーダービー 公式ポータルサイト｜Cygames](https://umamusume.jp/)
+- [【ウマ娘】基礎知識、リセマラ、攻略難易度　 \- えるあくの乱数調整](https://eruakudiary.hatenablog.com/entry/2021/02/27/220827)
+- [【ウマ娘攻略】サクラバクシンオー以外でも今夜勝ちたい『ウマ娘 プリティーダービー』【2021年2月号】 \| ゴジライン](https://goziline.com/archives/43607)
+- [【ウマ娘攻略】無課金でも勝ちまくりたいウマ娘トレーナー虎の巻【初心者向け】｜ルネ｜note](https://note.com/renekuroi/n/nfcec96121b63)
+
---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: 'Ataru Kodaka Site',
+    author: 'Ataru KODAKA',
+    email: 'ataru.kodaka@gmail.com',
+    description: 'THIS SITE IS ......WHATS THE FUNNY ?',
+    siteUrl: 'https://atarukodaka.github.io',
+    //siteUrl: 'https://cranky-williams-77ca16.netlify.app',
+    //siteUrl: `http://localhost:8000/`,
+    coverImage: '/images/top.png',
+    social: {
+      twitter: 'ataru_kodaka',
+      github: 'atarukodaka',
+    }  
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-aksite`,
+      options: {
+        contentPath: `content/posts`,
+
+        directoryLabels: {
+          "/workout": "ワークアウト",
+          "/game": "ゲーム",
+          "/game/kancolle": "艦これ",
+          "/game/kancolle/event": "イベント",
+          "/game/wot": "World of Tanks",
+          "/game/umamusume": "ウマ娘",
+          "/software": "ソフトウェア",
+          "/software/gatsby": "Gatsby",
+          "/software/middleman/susume": "Middlemanのすすめ",
+          "/figureskating": "フィギュアスケート",
+          "/figureskating/practise": "銀盤練習",
+          "/hobby": "趣味"        
+        }
+      }
+    }
+  ],
+}
