c302e886a5995006d18b25ba5d834b50b2469409
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-04-04 00:20:33 +0900

    deploy

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 
 
 deploy:
-	(cd ../; yarn workspace site build)
+	#(cd ../; yarn workspace site build)
 	rm -rf docs
-	mv public docs
+	#mv public docs
+	cp -r public docs
 	git add docs content
 	git commit -am deploy
 	git push origin master
--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b454844494e396f72ce7.js"
+    "url": "webpack-runtime-652372a2395195565d4e.js"
   },
   {
     "url": "framework-f79f8711c3cb2fe4f1e3.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "580fa87af06dcd7407c9ce4ac464c883"
+    "revision": "664b88746dc22dec5b803a7535bc64f3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-646b88f04b87f20cc34f.js"
