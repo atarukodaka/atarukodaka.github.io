4f5929ea145aa58c5cfc45a5ff8f4ebffed17014
Ataru KODAKA <ataru.kodaka@gmail.com>
2021-04-16 00:08:11 +0900

    deploy

--- docs/component---gatsby-theme-aksite-src-pages-404-js-a785c48dc468f7a13503.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunksite=self.webpackChunksite||[]).push([[490],{8867:function(t,u,e){"use strict";e.r(u);e(7378);var n=e(383),s=e(2861);u.default=function(){return(0,s.tZ)(n.Z,{title:"Not Found"},(0,s.tZ)("h1",null,"Not Found"))}}}]);
-//# sourceMappingURL=component---gatsby-theme-aksite-src-pages-404-js-a785c48dc468f7a13503.js.map
